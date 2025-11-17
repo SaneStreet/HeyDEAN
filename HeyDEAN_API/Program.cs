0d2a27721435d2e5d2300193ef3d068e4a9b5fa4
Michael Hansen <MichaelHansenIT@live.dk>
2025-11-17 14:51:54 +0100

    Added functionality * Connection string to appsettings * Packages for EFcore, Design and Tools * builder service for EF to Program

--- HeyDEAN_API/Program.cs
+++ HeyDEAN_API/Program.cs
@@ -1,8 +1,19 @@
+using HeyDEAN_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add EntityFramework
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 34))
+    )
+);
 
 var app = builder.Build();
 
