10ba97b2f78c9b95d1b220b18b9f9690a966396f
Michael Hansen <MichaelHansenIT@live.dk>
2025-11-19 11:13:19 +0100

    Refactored .csproj and Program.cs

--- HeyDEAN_API/Program.cs
+++ HeyDEAN_API/Program.cs
@@ -1,21 +1,32 @@
 using HeyDEAN_API.Data;
 using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
-// Add EntityFramework
+// Gets ConnectionString
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Adds DbContext, using MySQL
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(9, 4, 0))
-    )
-);
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
+
+// Sætter scope på hvilken database og migrationer der skal bruges
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+    // Tilføjer data fra .CSV ind i DB
+    //Seeder.SeedProducts(db, "Data/Cereal.csv");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,13 +35,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    
-    //Seeder.SeedAll(dbContext); // Seed the database and check if already seeded
-}
-
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
